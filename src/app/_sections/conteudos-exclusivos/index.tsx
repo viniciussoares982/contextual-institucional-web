db0beb1516a28f80f492cd63d254e3f65e52d668
Vinicius Soares <1165546@sga.pucminas.br>
2024-03-21 22:44:06 -0300

    organize elements on screen

--- src/app/_sections/conteudos-exclusivos/index.tsx
+++ src/app/_sections/conteudos-exclusivos/index.tsx
@@ -1,8 +1,22 @@
 export default function ConteudosExclusivosSection() {
 
   return (
-    <section>
-      <h2>ConteudosExclusivosSection</h2>
+    <section className="bg-black">
+      <div className="my-8">
+        <div className="max-w-8xl mx-auto px-7 sm:px-6 md:px-8 flex flex-col justify-end items-center h-full">
+          <div>
+            <span className="uppercase font-space-grotesk text-sm text-center block mb-12">Resumo breve</span>
+            <h2 className="text-4xl font-utopia-std font-normal text-center mb-12"><span className="text-contextual-pink block">Conteúdos</span> <span className="text-off-white block">exclusivos</span></h2>
+            <p className="text-sm font-space-grotesk text-off-white text-center mb-8">Donec elementum, lectus et vehicula auctor, orci risus viverra lacus, a posuere nisi nibh elementum nisl. In ac tincidunt justo.</p>
+          </div>
+          <div>
+            carousel
+          </div>
+          <div>
+            <a className="bg-contextual-pink text-black font-space-grotesk w-52 h-8 mb-4 flex justify-center items-center text-center rounded-md uppercase text-xs tracking-[.2em]">Ver Produtos</a>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
