e9f71d016d0c81022236c9c5a67bb9efd7c40546
mbertelle <junior@novaerainfo.com.br>
2024-03-21 21:32:40 -0300

    feat: new sections for hero and explain

--- src/app/_sections/explicar-descrever-controlar/index.tsx
+++ src/app/_sections/explicar-descrever-controlar/index.tsx
@@ -1,8 +1,16 @@
 export default function ExplicarDescreverControlarSection() {
 
   return (
-    <section>
-      <h2>ExplicarDescreverControlarSection</h2>
+    <section className="bg-transparent relative -top-6">
+      <div className="h-[632px] rounded-2xl" style={{background: 'linear-gradient(43.93deg, #AEA0FF 15.63%, #B6A1F7 17.47%, #CCA4E5 23.94%, #DDA6D7 32.26%, #E9A7CE 42.42%, #F0A8C8 56.28%, #F2A9C7 96.01%)'}}>
+        <div className="max-w-8xl mx-auto px-7 sm:px-6 md:px-8 flex flex-col justify-start items-center h-full py-9">
+          <div className="">
+            <p  className="text-black text-xs font-space-grotesk text-center uppercase mb-32 tracking-[.2em]">Explicar - Descrever - Controlar</p>
+            <h1 className="text-black text-4xl font-utopia-std font-semibold tracking-[-0.075em] text-center mb-32 h-56">“Aqui a ideia é ter uma mensagem de impacto que relacionada ao propósito ou transformacão que a contextual gera.”</h1>
+            <p  className="text-black text-xs font-space-grotesk text-center uppercase tracking-[.2em]">Pellentesque porta mi vel<span className="block">quam rhoncus</span> </p>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
