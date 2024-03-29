3b0abd748a5327365ba59c6e429a134a391bbf16
mbertelle <junior@novaerainfo.com.br>
2024-03-28 21:15:13 -0300

    feat: new sections video interlude and foot

--- src/app/_sections/explicar-descrever-controlar/index.tsx
+++ src/app/_sections/explicar-descrever-controlar/index.tsx
@@ -1,7 +1,7 @@
 export default function ExplicarDescreverControlarSection() {
 
   return (
-    <section className="bg-transparent relative -top-6">
+    <section className="bg-transparent">
       <div className="h-[632px] rounded-2xl" style={{background: 'linear-gradient(43.93deg, #AEA0FF 15.63%, #B6A1F7 17.47%, #CCA4E5 23.94%, #DDA6D7 32.26%, #E9A7CE 42.42%, #F0A8C8 56.28%, #F2A9C7 96.01%)'}}>
         <div className="max-w-8xl mx-auto px-7 sm:px-6 md:px-8 flex flex-col justify-start items-center h-full py-9">
           <div className="">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import HeroSection from "./_sections/hero-section";
 import ExplicarDescreverControlarSection from "./_sections/explicar-descrever-controlar";
 import VideoHomeSection from "./_sections/video-home";
+import Interlude from "./_sections/interlude";
 import AnuncioPlataformaSection from "./_sections/anuncio-plataforma";
 import ConteudosExclusivosSection from "./_sections/conteudos-exclusivos";
 import FaqSection from "./_sections/faq";
+import FootSection from "./_sections/foot";
 export default function HomePage() {
   return (
     <main>
       <HeroSection />
       <ExplicarDescreverControlarSection />
       <VideoHomeSection />
+      <Interlude />
       <AnuncioPlataformaSection />
       <ConteudosExclusivosSection />
       <FaqSection />
+      <FootSection />
     </main>
   );
 }
