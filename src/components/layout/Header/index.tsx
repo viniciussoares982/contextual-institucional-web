88c910a367f0183a5bc216a5a6bdeb05aafd0857
Vinicius Soares <1165546@sga.pucminas.br>
2024-04-15 23:06:19 -0300

    feat: creating menu header

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,7 +1,16 @@
+import Image from 'next/image'
+
 export default function Header() {
   return (
-    <nav className="max-w-8xl mx-auto flex h-full flex-col items-center justify-start px-7 sm:px-6 md:w-[722px] md:px-8">
-      asdasd
+    <nav className="absolute left-0 right-0">
+      <div className="flex justify-center p-4">
+        <Image
+          src="/logo-contextual.png"
+          width={184}
+          height={42}
+          alt="logo contextual"
+        />
+      </div>
     </nav>
   )
 }
