24566e4f243620390a216937a6a8f2ddba4b9589
Vinicius Soares <1165546@sga.pucminas.br>
2024-04-22 23:12:44 -0300

    creating hamb menu

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,15 +1,36 @@
+'use client'
 import Image from 'next/image'
+import React from 'react'
 
 export default function Header() {
+  const [isOpen, setIsOpen] = React.useState(false)
   return (
     <nav className="absolute left-0 right-0">
-      <div className="flex justify-center p-4">
+      <div className="flex justify-between p-4">
         <Image
           src="/logo-contextual.png"
           width={184}
           height={42}
           alt="logo contextual"
         />
+        <div
+          onClick={() => setIsOpen(!isOpen)}
+          id="nav-icon3"
+          className="relative mx-2 my-auto h-6 w-12 rotate-0 cursor-pointer transition-all duration-500 ease-in-out"
+        >
+          <span
+            className={`hamb-bar-last ${isOpen ? 'left-2/4 top-[12px] w-[0%]' : 'top-0 w-full'}`}
+          ></span>
+          <span
+            className={`hamb-bar ${isOpen ? 'rotate-45' : 'rotate-0'} top-[12px]`}
+          ></span>
+          <span
+            className={`hamb-bar ${isOpen ? '-rotate-45' : 'rotate-0'} top-[12px]`}
+          ></span>
+          <span
+            className={`hamb-bar-last ${isOpen ? 'left-1/2 top-[12px] w-[0%]' : 'top-6 w-full'}`}
+          ></span>
+        </div>
       </div>
     </nav>
   )
